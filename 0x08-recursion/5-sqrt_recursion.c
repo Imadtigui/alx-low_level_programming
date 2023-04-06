deb82c5d2a3564525c3d9f0b722240e5ac4f8941
imad tigui <imad.tigui@gmail.com>
2023-04-06 02:28:57 +0000

    recursion tsk

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,13 +3,13 @@ int sqrt(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
-	if (i *i == n)
-		return i;
+	if (i * i == n)
+		return (i);
 	return (sqrt(n, i + 1));
 }
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	return (sqrt(n, 0));
 }
