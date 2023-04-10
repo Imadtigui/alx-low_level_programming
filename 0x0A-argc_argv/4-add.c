5e823f8d2dd3d8ca628bf5f7c04a219406094800
imad tigui <imad.tigui@gmail.com>
2023-04-10 22:22:46 +0000

    tsk4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - func
+ * @argc: para
+ * @argv: para
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) > 0)
+			{
+				for (i = 1; i < argc; i++)
+				{
+					sum = sum + atoi(argv[i]);
+				}
+				printf("%d\n", sum);
+			}
+			else
+			{
+				printf("Error\n");
+				break;
+			}
+		}
+	}
+	else
+		printf("%d\n", 0);
+	return (0);
+}
+
