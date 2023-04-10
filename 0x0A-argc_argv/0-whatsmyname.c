56b8360ebc002230b26125f0d46ebea43dfd564c
imad tigui <imad.tigui@gmail.com>
2023-04-10 03:12:24 +0000

    tsk0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: para
+ * @argv: para
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("my name is %s\n", argv[0]);
+	return (0);
+}
