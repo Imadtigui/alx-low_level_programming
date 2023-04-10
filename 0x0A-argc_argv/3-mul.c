275e5d17cbfffad18b4f6a01889ecd3c0dc9c7ba
imad tigui <imad.tigui@gmail.com>
2023-04-10 17:31:19 +0000

    tsk2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - funct
+ * @argv: para
+ * @argc: para
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul = 1, i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+		printf("Error\n");
+
+	return (0);
+}
