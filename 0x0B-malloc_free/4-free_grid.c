33a34f80c3da6fac17ce81b9c022e1c46c2cece8
imad tigui <imad.tigui@gmail.com>
2023-04-12 01:58:57 +0000

    tsk4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_grid - fun
+ * @grid: para
+ * @height: para
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid[i]);
+}
