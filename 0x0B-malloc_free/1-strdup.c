9270126d1b52f56aba599bf0eaaf6e7376f5b651
imad tigui <imad.tigui@gmail.com>
2023-04-12 00:46:24 +0000

    tsk1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strdup - fun
+ * @str: para
+ *
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i = 0, siz = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[siz] != '\0'; siz++)
+		;
+	m = malloc(siz * sizeof(*str) + 1);
+	if (s == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; i < siz; i++)
+			s[i] = str[i];
+	}
+	return (s);
+}
