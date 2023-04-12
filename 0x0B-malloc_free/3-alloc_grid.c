b6d1f2df3501a2a056b7ceca3e4d6dcc8e3f6e34
imad tigui <imad.tigui@gmail.com>
2023-04-12 01:38:49 +0000

    tsk1

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * alloc_grid - fun
+ * @width: para
+ * @height: para
+ *
+ * Return: 0
+ */
+int **alloc_grid(int width, height)
+{
+	int **tb, i, j;
+
+	tab malloc(sizeof(*tab) * height);
+	if (width <= 0 || height <= 0 || tab == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			tb[i] = malloc(sizeof(**tb) * width);
+			if (tb[i] == 0)
+			{
+				while (i--)
+					free(tb[i]);
+				free(tb);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+				tab[i][j] = 0;
+		}
+	}
+	return (tab);
+}
