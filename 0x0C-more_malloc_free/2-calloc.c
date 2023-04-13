a21ce391bf032cd26e0ad9c021d9e280f55a90e8
imad tigui <imad.tigui@gmail.com>
2023-04-13 18:29:31 +0000

    tsk2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * mm - fun
+ * @s:para
+ * @b: para
+ * @n: para
+ *
+ * Return: ptr
+ */
+char *mm(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+	return (ptr);
+}
+/**
+ * _calloc - fun
+ * @nmemb: para
+ * @size: para
+ *
+ * Return: m
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	  void *m;
+
+	  if (size == 0 || nmemb == 0)
+		  return (NULL);
+	  m = malloc(sizeof(int) * nmemb);
+	  if (m == 0)
+		  return (NULL);
+	  mm(m, 0, sizeof(int) * nmemb);
+	  return (m);
+}
