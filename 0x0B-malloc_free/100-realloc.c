68564ce7c116c09c6464577b81a18f8753d63f2d
imad tigui <imad.tigui@gmail.com>
2023-04-14 20:57:05 +0000

    100

--- 0x0B-malloc_free/100-realloc.c
+++ 0x0B-malloc_free/100-realloc.c
@@ -1,22 +1,23 @@
 #include "main.h"
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int *p;
+	int *p, *i;
 
 	if (new_size <= old_size)
 	{
 		if (new_size == old_size)
-		return (ptr);
+		i = ptr;
 		else
 		{
 			ptr = malloc(sizeof(int) * old_size);
 			if (ptr == NULL)
 			{
 				p = realloc(ptr, sizeof(int) * new_size);
-				return (p);
+				i = p;
 			}
 			else if (new_size == 0 && ptr != NULL)
-				return (NULL);
+				i = NULL;
 		}
 	}
+	return i;
 }
