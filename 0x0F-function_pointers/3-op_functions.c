c8a68aae6d22638db84bc39596909156faf1c059
imad tigui <imad.tigui@gmail.com>
2023-04-20 00:32:00 +0000

    tsk2

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+/**
+ * op_add - fu
+ * @a: pa
+ * @b: pa
+ *
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - fu
+ * @a: pa
+ * @b: pa
+ *
+ * Return: 0
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - fu
+ * @a: pa
+ * @b: pa
+ *
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - fu
+ * @a: pa
+ * @b: pa
+ *
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - fu
+ * @a: pa
+ * @b: pa
+ *
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
